775c2cd484121a05629638cd4d691416e21cb907
RezaPahlevii <rezapahlevih2o@gmail.com>
2023-04-19 08:31:18 +0700

    connect session squelize

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,19 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 import dotenv from "dotenv";
-// import db from "./config/Database.js";
+import db from "./config/Database.js";
+import SequelizeStore from "connect-session-sequelize";
 import UserRoute from "./routes/UserRoute.js";
 import KostRoute from "./routes/KostRoute.js";
+import AuthRoute from "./routes/AuthRoute.js";
 dotenv.config();
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+const store = new sessionStore({
+    db: db
+});
 
 /**Genarate table otomatis di database kost_db */
 // (async()=>{
@@ -18,6 +25,7 @@ app.use(session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized: true,
+    store: store,
     cookie: {
         secure: 'auto'
     }
@@ -31,6 +39,9 @@ app.use(cors({
 app.use(express.json());
 app.use(UserRoute);
 app.use(KostRoute);
+app.use(AuthRoute);
+
+// store.sync();
 
 app.listen(process.env.APP_PORT, ()=>{
     console.log('Server berjalan dengan baik..');
