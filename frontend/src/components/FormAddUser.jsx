1a6192b80a826574916f20129f36311377ec0b96
RezaPahlevii <rezapahlevi.school@gmail.com>
2023-06-06 21:58:17 +0700

    add user (problem)

--- frontend/src/components/FormAddUser.jsx
+++ frontend/src/components/FormAddUser.jsx
@@ -1,63 +1,115 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const FormAddUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confpassword, setConfPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+
+  const saveUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/users", {
+        name: name,
+        email: email,
+        password: password,
+        confpassword: confpassword,
+        role: role
+      });
+      navigate("/users");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
+
   return (
     <div>
-        <h1 className='title'>Users</h1>
-        <h2 className='subtitle'>Add New User</h2>
-        <div className="card is-shadowless">
-            <div className="card-content">
-                <div className="content">
-                <form >
-               
-                    <div className="field">
-                        <label className="label">Name</label>
-                        <div className="control">
-                            <imput type="text" className="input" placeholer='Name'/>
-                        </div>
-                    </div>
-                    <div className="field">
-                        <label className="label">Email</label>
-                        <div className="control">
-                            <imput type="text" className="input" placeholer='Email'/>
-                        </div>
-                    </div>
-                    <div className="field">
-                        <label className="label">Password</label>
-                        <div className="control">
-                            <imput type="password" className="input" placeholer='******'/>
-                        </div>
-                    </div>
-                    <div className="field">
-                        <label className="label">Confirm Password</label>
-                        <div className="control">
-                            <imput type="password" className="input" placeholer='******'/>
-                        </div>
-                    </div>
-                    <form >
-                    <div className="field">
-                        <label className="label">Role</label>
-                        <div className="control">
-                            <div className="select is-fullwidth">
-                                <select>
-                                    <option value="admin">Admin</option>
-                                    <option value="user">user</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="field">
-                        <div className="control">
-                        <button className="button is-success">Save</button>
-                        </div>
-                    </div>
-                </form>
-                </form>
+      <h1 className="title">Users</h1>
+      <h2 className="subtitle">Add New User</h2>
+      <div className="card is-shadowless">
+        <div className="card-content">
+          <div className="content">
+            <form onSubmit={saveUser}>
+                <p className="has-text-centered">{msg}</p>
+              <div className="field">
+                <label className="label">Name</label>
+                <div className="control">
+                  <input
+                    type="text"
+                    className="input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholer="Name"
+                  />
                 </div>
-            </div>
+              </div>
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control">
+                  <input
+                    type="text"
+                    className="input"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholer="Email"
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Password</label>
+                <div className="control">
+                  <input
+                    type="password"
+                    className="input"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholer="******"
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Confirm Password</label>
+                <div className="control">
+                  <input
+                    type="password"
+                    className="input"
+                    value={confpassword}
+                    onChange={(e) => setConfPassword(e.target.value)}
+                    placeholer="******"
+                  />
+                </div>
+              </div>
+              <form>
+                <div className="field">
+                  <label className="label">Role</label>
+                  <div className="control">
+                    <div className="select is-fullwidth">
+                      <select value={role} onChange={(e)=> setRole(e.target.value)}>
+                        <option value="admin">Admin</option>
+                        <option value="user">user</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="control">
+                    <button type="submit" className="button is-success">Save</button>
+                  </div>
+                </div>
+              </form>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FormAddUser
+export default FormAddUser;
