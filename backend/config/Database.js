6d5c9b10dca928ab47392db84310284ca1abaacb
RezaPahlevii <rezapahlevi.school@gmail.com>
2023-06-17 13:43:54 +0700

    change variabel db

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,12 @@
 // import { Sequelize } from "sequelize";
 const { Sequelize } = require('sequelize')
 
-const db = new Sequelize('u1563728_kost_db', 'u1563728_admin', '8VQS=SX$HF?r', {
-    host: "api.bengkaliskost.com",
+// const db = new Sequelize('u1563728_kost_db', 'u1563728_admin', '8VQS=SX$HF?r', {
+//     host: "api.bengkaliskost.com",
+//     dialect: "mysql"
+// });
+const db = new Sequelize('kost_db', 'root', '', {
+    host: "localhost",
     dialect: "mysql"
 });
 
