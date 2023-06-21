8f45d918c41093f9ce31fbfbee890cf6fde91313
RezaPahlevii <rezapahlevi.school@gmail.com>
2023-06-21 13:45:12 +0700

    new field tblkost

--- backend/controllers/Kost.js
+++ backend/controllers/Kost.js
@@ -2,159 +2,256 @@ import Kost from "../models/KostModel.js";
 import Users from "../models/UserModel.js";
 import { Op } from "sequelize";
 
- export const getKost = async (req, res) =>{
-    try {
-        let response;
-        if(req.role === "admin"){
-            response = await Kost.findAll({
-                attributes:['uuid','name','price'],
-                include:[{
-                    model: Users,
-                    attributes:['name','email']
-                }]
-            });
-        }else{
-            response = await Kost.findAll({
-                attributes:['uuid','name','price'],
-                where:{
-                    userId: req.userId
-                },
-                include:[{
-                    model: Users,
-                    attributes:['name','email']
-                }]
-            });
-        }
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({msg: error.massage});
-    }
- }
-
- export const getKostById = async(req, res)=>{
-    try {
-        const kost = await Kost.findOne({
-            where:{
-                uuid: req.params.id
-            }
-        });
-        if(!kost) return res.status(404).json({msg: "Data tidak ditemukan"});
-        let response;
-        if(req.role === "admin"){
-            response = await Kost.findOne({
-                attributes:['uuid','name','price'],
-                where:{
-                    id: kost.id
-                },
-                include:[{
-                    model: Users,
-                    attributes:['name','email']
-                }]
-            });
-        }else{
-            response = await Kost.findOne({
-                attributes:['uuid','name','price'],
-                where:{
-                    [Op.and]:[{id: kost.id}, {userId: req.userId}]
-                },
-                include:[{
-                    model: Users,
-                    attributes:['name','email']
-                }]
-            });
-        }
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({msg: error.massage});
-    }
- }
-
- export const createKost = async(req, res) =>{
-    const  {name, price} = req.body;
-    try {
-        await Kost.create({
-            name: name,
-            price: price,
-            userId: req.userId
-        });
-        res.status(201).json({msg: "Berhasil menambahkan kamar kost"});
-    } catch (error) {
-        res.status(500).json({msg: error.massage});
-    }
- }
-
- export const updateKost = async(req, res) =>{
-    try {
-        const kost = await Kost.findOne({
-            where:{
-                uuid: req.params.id
-            }
-        });
-        if(!kost) return res.status(404).json({msg: "Data tidak ditemukan"});
-        const  {name, price} = req.body;
-        if(req.role === "admin"){
-            await Kost.update({name, price},{
-                where:{
-                    id: kost.id
-                }
-            });
-        }else{
-            if(req.userId !== kost.userId) return res.status(403).json({msg: "Akses terlarang"})
-            await Kost.update({name, price},{
-                where:{
-                    [Op.and]:[{id: kost.id}, {userId: req.userId}]
-                }
-            });
-        }
-        res.status(200).json({msg: "Berhasil update kost"});
-    } catch (error) {
-        res.status(500).json({msg: error.massage});
-    }
- }
-
- export const deleteKost = async(req, res) =>{
-    try {
-        const kost = await Kost.findOne({
-            where:{
-                uuid: req.params.id
-            }
-        });
-        if(!kost) return res.status(404).json({msg: "Data tidak ditemukan"});
-        const  {name, price} = req.body;
-        if(req.role === "admin"){
-            await Kost.destroy({
-                where:{
-                    id: kost.id
-                }
-            });
-        }else{
-            if(req.userId !== kost.userId) return res.status(403).json({msg: "Akses terlarang"})
-            await Kost.destroy({
-                where:{
-                    [Op.and]:[{id: kost.id}, {userId: req.userId}]
-                }
-            });
-        }
-        res.status(200).json({msg: "Berhasil delete kost"});
-    } catch (error) {
-        res.status(500).json({msg: error.massage});
-    }
- }
-
- export const getRekomendasiKost = async (req, res) => {
-    try {
-        let response;
-      // Implementasi logika untuk mengambil data rekomendasi kost
-      response = await Kost.findAll({ 
-        attributes:['uuid','name','price'],
-        include: {
+export const getKost = async (req, res) => {
+  try {
+    let response;
+    if (req.role === "admin") {
+      response = await Kost.findAll({
+        attributes: ["uuid", "name", "price"],
+        include: [
+          {
             model: Users,
-            attributes: ['name'],
+            attributes: ["name", "email"],
           },
-       });
-      res.json(response);
-    } catch (error) {
-      res.status(500).json({ error: "Internal server error" });
+        ],
+      });
+    } else {
+      response = await Kost.findAll({
+        attributes: ["uuid", "name", "price"],
+        where: {
+          userId: req.userId,
+        },
+        include: [
+          {
+            model: Users,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
     }
-  };
-  
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.massage });
+  }
+};
+
+export const getKostById = async (req, res) => {
+  try {
+    const kost = await Kost.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!kost) return res.status(404).json({ msg: "Data tidak ditemukan" });
+    let response;
+    if (req.role === "admin") {
+      response = await Kost.findOne({
+        attributes: ["uuid", "name", "price"],
+        where: {
+          id: kost.id,
+        },
+        include: [
+          {
+            model: Users,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
+    } else {
+      response = await Kost.findOne({
+        attributes: ["uuid", "name", "price"],
+        where: {
+          [Op.and]: [{ id: kost.id }, { userId: req.userId }],
+        },
+        include: [
+          {
+            model: Users,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
+    }
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.massage });
+  }
+};
+
+export const createKost = async (req, res) => {
+  const {
+    name,
+    no_hp,
+    desa,
+    alamat,
+    gender,
+    f_kamar,
+    s_kamar,
+    f_kamar_mandi,
+    f_umum,
+    f_parkir,
+    peraturan_kost,
+    cerita_pemmilik_kost,
+    catatan_tambahan,
+    maps,
+    foto_kost,
+    price,
+  } = req.body;
+  try {
+    await Kost.create({
+      name: name,
+      no_hp: no_hp,
+      desa: desa,
+      alamat: alamat,
+      gender: gender,
+      f_kamar: f_kamar,
+      s_kamar: s_kamar,
+      f_kamar_mandi: f_kamar_mandi,
+      f_umum: f_umum,
+      f_parkir: f_parkir,
+      peraturan_kost: peraturan_kost,
+      cerita_pemmilik_kost,
+      catatan_tambahan,
+      maps,
+      foto_kost,
+      price,
+      userId: req.userId,
+    });
+    res.status(201).json({ msg: "Berhasil menambahkan kamar kost" });
+  } catch (error) {
+    res.status(500).json({ msg: error.massage });
+  }
+};
+
+export const updateKost = async (req, res) => {
+  try {
+    const kost = await Kost.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!kost) return res.status(404).json({ msg: "Data tidak ditemukan" });
+    const {
+      name,
+      no_hp,
+      desa,
+      alamat,
+      gender,
+      f_kamar,
+      s_kamar,
+      f_kamar_mandi,
+      f_umum,
+      f_parkir,
+      peraturan_kost,
+      cerita_pemmilik_kost,
+      catatan_tambahan,
+      maps,
+      foto_kost,
+      price,
+    } = req.body;
+    if (req.role === "admin") {
+      await Kost.update(
+        {
+          name,
+          no_hp,
+          desa,
+          alamat,
+          gender,
+          f_kamar,
+          s_kamar,
+          f_kamar_mandi,
+          f_umum,
+          f_parkir,
+          peraturan_kost,
+          cerita_pemmilik_kost,
+          catatan_tambahan,
+          maps,
+          foto_kost,
+          price,
+        },
+        {
+          where: {
+            id: kost.id,
+          },
+        }
+      );
+    } else {
+      if (req.userId !== kost.userId)
+        return res.status(403).json({ msg: "Akses terlarang" });
+      await Kost.update(
+        {
+          name,
+          no_hp,
+          desa,
+          alamat,
+          gender,
+          f_kamar,
+          s_kamar,
+          f_kamar_mandi,
+          f_umum,
+          f_parkir,
+          peraturan_kost,
+          cerita_pemmilik_kost,
+          catatan_tambahan,
+          maps,
+          foto_kost,
+          price,
+        },
+        {
+          where: {
+            [Op.and]: [{ id: kost.id }, { userId: req.userId }],
+          },
+        }
+      );
+    }
+    res.status(200).json({ msg: "Berhasil update kost" });
+  } catch (error) {
+    res.status(500).json({ msg: error.massage });
+  }
+};
+
+export const deleteKost = async (req, res) => {
+  try {
+    const kost = await Kost.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!kost) return res.status(404).json({ msg: "Data tidak ditemukan" });
+    const { name, price } = req.body;
+    if (req.role === "admin") {
+      await Kost.destroy({
+        where: {
+          id: kost.id,
+        },
+      });
+    } else {
+      if (req.userId !== kost.userId)
+        return res.status(403).json({ msg: "Akses terlarang" });
+      await Kost.destroy({
+        where: {
+          [Op.and]: [{ id: kost.id }, { userId: req.userId }],
+        },
+      });
+    }
+    res.status(200).json({ msg: "Berhasil delete kost" });
+  } catch (error) {
+    res.status(500).json({ msg: error.massage });
+  }
+};
+
+export const getRekomendasiKost = async (req, res) => {
+  try {
+    let response;
+    // Implementasi logika untuk mengambil data rekomendasi kost
+    response = await Kost.findAll({
+      attributes: ["uuid", "name", "price"],
+      include: {
+        model: Users,
+        attributes: ["name"],
+      },
+    });
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/models/KostModel.js
+++ backend/models/KostModel.js
@@ -2,44 +2,137 @@ import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
 import Users from "./UserModel.js";
 
-const {DataTypes} = Sequelize;
-const Kost = db.define('kost',{
-   uuid:{
-       type: DataTypes.STRING,
-       defaultValue: DataTypes.UUIDV4,
-       allowNull: false,
-       validate:{
-           notEmpty: true
-       }
-   },
-   name:{
-       type: DataTypes.STRING,
-       allowNull: false,
-       validate:{
-           notEmpty: true,
-           len: [3, 100]
-       }
-   },
-   price:{
-       type: DataTypes.INTEGER,
-       allowNull: false,
-       validate:{
-           notEmpty: true,
-       }
-   },
-   userId:{
-       type: DataTypes.INTEGER,
-       allowNull: false,
-       validate:{
-           notEmpty: true,
-       }
-   },
-},
-{
-freezeTableName: true
-});
+const { DataTypes } = Sequelize;
+const Kost = db.define(
+  "kost",
+  {
+    uuid: {
+      type: DataTypes.STRING,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [3, 100],
+      },
+    },
+    no_hp: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [9, 15],
+      },
+    },
+    desa: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    alamat: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    f_kamar: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    s_kamar: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    f_kamar_mandi: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    f_umum: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    f_parkir: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    peraturan_kost: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    cerita_pemmilik_kost: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    catatan_tambahan: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    maps: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    foto_kost: {
+      type: DataTypes.BLOB,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
 
 Users.hasMany(Kost);
-Kost.belongsTo(Users, {foreignKey: 'userId'});
+Kost.belongsTo(Users, { foreignKey: "userId" });
 
 export default Kost;
