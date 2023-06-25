98d16a05b8434a398a0e00b7c6d106328ddb3a27
RezaPahlevii <rezapahlevi.school@gmail.com>
2023-06-25 18:20:02 +0700

    consume API & layout

--- frontend/src/pages/ListKost.jsx
+++ frontend/src/pages/ListKost.jsx
@@ -1,69 +1,72 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Card, CardImg, Col, Container, Row } from "react-bootstrap";
 import FilterFasilitas from "../components/FilterFasilitas";
-import Footer from "../components/Footer";
-import KostList from "../components/KostList";
+// import KostList from "../components/KostList";
 import Nav2 from "../components/Nav2";
 import Maps from "./../components/Maps";
-import Rumah from "./../image/rumah.jpg";
-import Rumah2 from "./../image/rumah2.jpg";
-import Rumah3 from "./../image/rumah3.jpg";
-import Rumah4 from "./../image/rumah4.jpg";
+import axios from "axios";
+import Footer2 from "../components/Footer2";
+import rumah from "./../image/rumah.jpg";
+import "../css/ListKost.css";
+// import Rumah2 from "./../image/rumah2.jpg";
+// import Rumah3 from "./../image/rumah3.jpg";
+// import Rumah4 from "./../image/rumah4.jpg";
 
 const ListKost = () => {
+  const [kosts, setKosts] = useState([]);
+
+  const getRekomendasiKosts = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/rekomendasi-kost"
+      );
+      setKosts(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getRekomendasiKosts();
+  }, []);
+
   return (
     <div>
-      <Container>
-        <Row>
+      <>
           <Nav2 />
-        </Row>
-        <Row className="mt-4 pt-3">
-          <Col md={6}>
-            <FilterFasilitas />
-            <KostList
-              img={Rumah}
-              kost="Kost Hijau"
-              alamat="Jl. Bathin Alam GG. AMD"
-              desa="Desa Sungai Alam"
-              harga="Rp. 200.000 / bulan"
-            />
-            <KostList
-              img={Rumah2}
-              kost="Kost Kuning"
-              alamat="Jl. Bathin Alam GG. AMD"
-              desa="Desa Sungai Alam"
-              harga="Rp. 500.000 / bulan"
-            />
-            <KostList
-              img={Rumah3}
-              kost="Kost Pink"
-              alamat="Jl. Bathin Alam GG. AMD"
-              desa="Desa Sungai Alam"
-              harga="Rp. 300.000 / bulan"
-            />
-            <KostList
-              img={Rumah4}
-              kost="Kost Biru"
-              alamat="Jl. Bathin Alam GG. AMD"
-              desa="Desa Sungai Alam"
-              harga="Rp. 500.000 / bulan"
-            />
-            <KostList
-              img={Rumah2}
-              kost="Kost Ungu"
-              alamat="Jl. Bathin Alam GG. AMD"
-              desa="Desa Sungai Alam"
-              harga="Rp. 700.000 / bulan"
-            />
-          </Col>
-          <Col>
-            <Maps />
-          </Col>
-        </Row>
-        <Row className="mt-3">
-          <Footer />
-        </Row>
-      </Container>
+      </>
+      <Container className="pt-5">
+      <FilterFasilitas />
+  <Row>
+    <Col xs={12} lg={7}>
+      <div className="card-container">
+        {kosts.map((kost, index) => (
+          <Card key={kost.uuid} className="mb-3">
+            <Row>
+              <Col xs={5}>
+                <CardImg variant="top" src={rumah} />
+              </Col>
+              <Col xs={7}>
+                <Card.Body>
+                  <Card.Title>{kost.nama}</Card.Title>
+                  <Card.Text>{kost.harga}</Card.Text>
+                  <Card.Text>{kost.user.name}</Card.Text>
+                  <Card.Text>{kost.f_kamar.join(", ")}</Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+        ))}
+      </div>
+    </Col>
+    <Col className="" xs={12} lg={5}>
+      <Maps />
+    </Col>
+  </Row>
+</Container>
+      <>
+        <Footer2 />
+      </>
     </div>
   );
 };
