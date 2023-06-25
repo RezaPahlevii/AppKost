557f9e16794c6f28e1aa87b07606bc0a69a6bb32
RezaPahlevii <rezapahlevi.school@gmail.com>
2023-06-25 14:31:42 +0700

    funtion searchbar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardImg, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, CardImg, Col, Container, Form, Row } from "react-bootstrap";
 // import RekomendasiKost from "../components/RekomendasiKost";
 import rumah from "./../image/rumah.jpg";
-import rumah2 from "./../image/rumah2.jpg";
+// import rumah2 from "./../image/rumah2.jpg";
+// import Banner from "../components/Banner";
 // import rumah3 from "./../image/rumah3.jpg";
 // import rumah4 from "./../image/rumah4.jpg";
 import Nav2 from "./../components/Nav2";
-import Banner from "../components/Banner";
-import SearchBar from "../components/SearchBar";
+// import SearchBar from "../components/SearchBar";
 import Footer2 from "../components/Footer2";
 import axios from "axios";
 // import { Link } from "react-router-dom";
 
 const HomePage = () => {
   const [kosts, setKosts] = useState([]);
+  const [search, setSearch] = useState("");
+  console.log(search);
 
   const getRekomendasiKosts = async () => {
     try {
@@ -34,12 +36,28 @@ const HomePage = () => {
     <div>
       <Nav2 />
       <Container className="my-5 py-5">
-        <div className="pb-5">
-          <SearchBar />
+      <div className="pb-5 search-container">
+          <h2>
+            <strong>Temukan Kost Kebutuhan Anda</strong>
+          </h2>
+          <p> Dapatkan infonya dan langsung sewa di AppKost</p>
+          <Col sm={4}>
+            <Form sticky="top" className="d-flex pt-2 pb-5">
+              <Form.Control
+                onChange={(e) => setSearch(e.target.value)}
+                type="search"
+                placeholder="Masukkan nama kost/alamat/area"
+                className="me-2"
+                aria-label="Search"
+                style={{ height: "2.5rem", width: "2" }}
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Col>
         </div>
-        <div>
+        {/* <div>
           <Banner banner={rumah2} />
-        </div>
+        </div> */}
         <div className="row mt-5 pt-5">
           <div>
             <h3 className="pb-4">
@@ -49,18 +67,36 @@ const HomePage = () => {
             </h3>
           </div>
           <Row>
-            {kosts.map((kost, index) => (
-              <Col key={kost.uuid} xs={12} sm={6} md={4} lg={3}>
-                <Card className="mb-3">
-                  <CardImg variant="top" src={rumah} />
-                  <Card.Body>
-                    <Card.Title>{kost.nama}</Card.Title>
-                    <Card.Text>{kost.harga}</Card.Text>
-                    <Card.Text>{kost.user.name}</Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
+          {kosts
+              .filter((kost) => {
+                const searchLower = search.toLowerCase();
+                const nameLower = kost.nama.toLowerCase();
+                const priceLower = kost.harga.toString().toLowerCase();
+                const ownerNameLower = kost.user.name.toLowerCase();
+                const filterFasilitas = kost.f_kamar.find((fasilitas) =>
+                fasilitas.toLowerCase().includes(searchLower)
+              );
+
+                return search.toLowerCase() === "" ||
+                nameLower.includes(searchLower) ||
+                priceLower.includes(searchLower) ||
+                ownerNameLower.includes(searchLower) ||
+                filterFasilitas;
+                
+              })
+              .map((kost, index) => (
+                <Col key={kost.uuid} xs={12} sm={6} md={4} lg={3}>
+                  <Card className="mb-3">
+                    <CardImg variant="top" src={rumah} />
+                    <Card.Body>
+                      <Card.Title>{kost.nama}</Card.Title>
+                      <Card.Text>{kost.harga}</Card.Text>
+                      <Card.Text>{kost.user.name}</Card.Text>
+                      <Card.Text>{kost.f_kamar}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
           </Row>
 
           {/* <div className="col-4">
