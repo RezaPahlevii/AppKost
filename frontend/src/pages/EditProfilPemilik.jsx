6eee658c91f4d4b00208b48f3a5bf9cdcc6b0cec
RezaPahlevii <rezapahlevi.school@gmail.com>
2023-07-18 15:42:09 +0700

    biodata byId

--- frontend/src/pages/EditProfilPemilik.jsx
+++ frontend/src/pages/EditProfilPemilik.jsx
@@ -1,141 +1,173 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import axios from "axios";
+import { getMe } from "../features/authSlice";
+import { useNavigate, useParams } from "react-router-dom";
 import Layout from "./Layout";
+import { useDispatch, useSelector } from "react-redux";
 
-const EditProfilPemilik = () => {
-  const [isEditing, setIsEditing] = useState(true); // State untuk menandai apakah sedang dalam mode editing
+const FormBiodataPenyewa = () => {
+  const { isError } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const [nama, setNama] = useState("");
+  const [jk, setJK] = useState("");
+  const [umur, setUmur] = useState("");
+  const [NoWA, setNoWA] = useState("");
+  const [asal, setAsal] = useState("");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-  const handleEditClick = () => {
-    setIsEditing(true);
-  };
+  useEffect(() => {
+    dispatch(getMe());
+  }, [dispatch]);
 
-  const handleSaveClick = (e) => {
+  useEffect(() => {
+    if (isError) {
+      navigate("/");
+    }
+  }, [isError, navigate]);
+
+  // Tampilkan data form edit sesuai record database
+  useEffect(() => {
+    const getBioUsersById = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/biodata/${id}`
+        );
+        setNama(response.data.nama);
+        setJK(response.data.jk);
+        setUmur(response.data.umur);
+        setNoWA(response.data.NoWA);
+        setAsal(response.data.asal);
+      } catch (error) {
+        if (error.response) {
+          setMsg("Silahkan lengkapi biodata anda!");
+        }
+      }
+    };
+    getBioUsersById();
+  }, [id]);
+
+  const saveBio = async (e) => {
     e.preventDefault();
-    // Logika untuk menyimpan data profil pemilik kost
-    setIsEditing(false);
+    try {
+      await axios.post("http://localhost:5000/biodata", {
+        nama: nama,
+        jk: jk,
+        umur: umur,
+        NoWA: NoWA,
+        asal: asal,
+      });
+      navigate("/dashboard");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
   };
 
   return (
-    <Layout>
-      <div className="hero-body">
-        <div className="container">
-          <div className="columns is-centered">
-            <div className="column is-8">
-              <h3 className="title text-center">Profil Pemilik Kost</h3>
-              <div className="card is-shadowless">
-                <div className="card-content">
-                  <div className="content">
-                    <form>
-                      <div className="field">
-                        <label className="label">Name</label>
-                        <div className="control">
-                          <input
-                            type="text"
-                            className="input"
-                            placeholder="Name"
-                            disabled={!isEditing}
-                          />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <label className="label">Jenis Kelamin</label>
-                        <div className="control">
-                          <div className="select is-fullwidth">
-                            <select
-                              disabled={!isEditing}
-                              defaultValue="Laki-laki"
-                            >
-                              <option value="Laki-laki">Laki-laki</option>
-                              <option value="Perempuan">Perempuan</option>
-                            </select>
+    <div>
+      <Layout>
+        <Container className="pt-3">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns is-centered">
+                <div className="column is-8">
+                  <h3 className="title text-center">Biodata</h3>
+                  <div className="card is-shadowless">
+                    <div className="card-content">
+                      <div className="content">
+                        <form onSubmit={saveBio}>
+                          <p className="has-text-centered">{msg}</p>
+                          <div className="field">
+                            <label className="label">Nama</label>
+                            <div className="control">
+                              <input
+                                type="text"
+                                className="input"
+                                value={nama}
+                                onChange={(e) => setNama(e.target.value)}
+                                placeholder="Nama"
+                              />
+                            </div>
                           </div>
-                        </div>
-                      </div>
-                      <div className="field">
-                        <label className="label">Umur</label>
-                        <div className="control">
-                          <input
-                            type="text"
-                            className="input"
-                            placeholder="25 Tahun"
-                            disabled={!isEditing}
-                          />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <label className="label">No HP</label>
-                        <div className="control">
-                          <input
-                            type="text"
-                            className="input"
-                            placeholder="Nomor Handphone"
-                            disabled={!isEditing}
-                          />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <label className="label">No WA</label>
-                        <div className="control">
-                          <input
-                            type="text"
-                            className="input"
-                            placeholder="Nomor Whatsapp"
-                            disabled={!isEditing}
-                          />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <label className="label">
-                          Alamat Rumah Sekarang
-                        </label>
-                        <div className="control">
-                          <input
-                            type="text"
-                            className="input"
-                            placeholder="Alamat rumah yang anda tempati sekarang "
-                            disabled={!isEditing}
-                          />
-                        </div>
-                      </div>
-                      <div className="field">
-                        <div className="control">
-                          {!isEditing ? (
-                            <button
-                              type="button"
-                              className="button is-success"
-                              onClick={handleEditClick}
-                            >
-                              Ubah
-                            </button>
-                          ) : (
-                            <>
+                          <div className="field">
+                            <label className="label">Jenis Kelamin</label>
+                            <div className="control">
+                              <div className="select is-fullwidth">
+                                <select
+                                  value={jk}
+                                  onChange={(e) => setJK(e.target.value)}
+                                >
+                                  <option hidden>Jenis Kelamin</option>
+                                  <option value="Laki-laki">Laki-laki</option>
+                                  <option value="Perempuan">Perempuan</option>
+                                </select>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="field">
+                            <label className="label">Umur</label>
+                            <div className="control">
+                              <input
+                                type="text"
+                                className="input"
+                                value={umur}
+                                onChange={(e) => setUmur(e.target.value)}
+                                placeholder="25 Tahun"
+                              />
+                            </div>
+                          </div>
+                          <div className="field">
+                            <label className="label">No WA</label>
+                            <div className="control">
+                              <input
+                                type="text"
+                                className="input"
+                                value={NoWA}
+                                onChange={(e) => setNoWA(e.target.value)}
+                                placeholder="Nomor Whatsapp"
+                              />
+                            </div>
+                          </div>
+                          <div className="field">
+                            <label className="label">Asal Daerah</label>
+                            <div className="control">
+                              <input
+                                type="text"
+                                className="input"
+                                value={asal}
+                                onChange={(e) => setAsal(e.target.value)}
+                                placeholder="nama daerah anda berasal "
+                              />
+                            </div>
+                          </div>
+                          <div className="field">
+                            <div className="control">
                               <button
                                 type="submit"
+                                onClick={saveBio}
                                 className="button is-success"
-                                onClick={handleSaveClick}
                               >
                                 Simpan
                               </button>
-                              <button
-                                type="button"
-                                className="button is-warning"
-                                onClick={() => setIsEditing(false)}
-                              >
-                                Batal
-                              </button>
-                            </>
-                          )}
-                        </div>
+                            </div>
+                          </div>
+                        </form>
                       </div>
-                    </form>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </Layout>
+        </Container>
+      </Layout>
+    </div>
   );
 };
 
-export default EditProfilPemilik;
+export default FormBiodataPenyewa;
