f88cfd4a5c1b7d8dbf3b0dc69468a1a45b2a198c
RezaPahlevii <rezapahlevi.school@gmail.com>
2023-07-24 20:36:45 +0700

    nav responsive

--- frontend/src/components/Nav2.jsx
+++ frontend/src/components/Nav2.jsx
@@ -1,33 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LogOut, reset, getMe } from "../features/authSlice";
-import {
-  Container,
-  Navbar,
-  Form,
-  Button,
-} from "react-bootstrap";
+import { Container, Navbar, Form, Button, NavDropdown } from "react-bootstrap";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
 const Nav = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const {user} = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.auth);
+  const [kosts, setKosts] = useState([]);
 
-  const logout = () =>{
+  const logout = () => {
     dispatch(LogOut());
     dispatch(reset());
     navigate("/");
   };
-  const login = () =>{
+  const login = () => {
     dispatch(getMe());
     dispatch(reset());
     navigate("/login");
   };
-  const dashboard =()=>{
-    navigate("/dashboard")
-  }
-  
+  const dashboard = () => {
+    navigate("/dashboard");
+  };
+
+  useEffect(() => {
+    getKosts();
+  }, []);
+  const getKosts = async () => {
+    const response = await axios.get("http://localhost:5000/rumah-kost");
+    setKosts(response.data);
+  };
+
   return (
     <div className="mb-5 ">
       <Navbar
@@ -38,30 +43,84 @@ const Nav = () => {
         variant="light"
       >
         <Container>
-          <NavLink
-            style={{ textDecoration: "none", color: "Black" }}
-            to={"/"}
-          >
+          <NavLink style={{ textDecoration: "none", color: "Black" }} to={"/"}>
             AppKost
           </NavLink>
-          <Navbar.Collapse className="justify-content-end"></Navbar.Collapse>
-          <NavLink
-            style={{ textDecoration: "none", paddingRight: 15, color: "Black" }}
-            to={"/pusat-bantuan"}
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="justify-content-end"
           >
-            Pusat Bantuan
-          </NavLink>
-          <Form className="d-flex">
-           {user == null ? (
-            <Button onClick={login} variant="outline-success">Login</Button>
-           ) : (
-            <>
-            <Button onClick={logout} variant="outline-success">Logout</Button>
-            <Button onClick={dashboard} variant="outline-success">avatar</Button>
-            </>
-           )
-        }
-          </Form>
+            <NavLink
+              style={{
+                textDecoration: "none",
+                paddingRight: 15,
+                color: "Black",
+              }}
+              className="mx-2"
+              to={"/pusat-bantuan"}
+            >
+              Pusat Bantuan
+            </NavLink>
+            <NavLink
+              style={{
+                textDecoration: "none",
+                paddingRight: 15,
+                color: "Black",
+              }}
+              className="mx-2"
+              to={"/tentang-kami"}
+            >
+              Tentang Kami
+            </NavLink>
+            <NavLink
+              style={{
+                textDecoration: "none",
+                paddingRight: 15,
+                color: "Black",
+              }}
+              className="mx-2"
+              to={"/maps"}
+            >
+              Maps
+            </NavLink>
+            <NavLink
+              style={{
+                textDecoration: "none",
+                paddingRight: 15,
+                color: "Black",
+              }}
+              className="mx-2"
+              to={"/kost-list"}
+            >
+              List Rumah Kost
+            </NavLink>
+            <Form className="d-flex">
+              {user == null ? (
+                <Button onClick={login} variant="outline-success">
+                  Login
+                </Button>
+              ) : (
+                <>
+                  <NavDropdown title={user.name} id="collasible-nav-dropdown">
+                    <NavDropdown.Item
+                      onClick={dashboard}
+                      variant="outline-success"
+                    >
+                      Akun
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item
+                      onClick={logout}
+                      variant="outline-success"
+                    >
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              )}
+            </Form>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
