f406f93e5474302ffbd50a042ffa80e1b32866bb
RezaPahlevii <rezapahlevi.school@gmail.com>
2023-07-25 11:47:25 +0700

    fixed responsive navbar

--- frontend/src/components/Nav2.jsx
+++ frontend/src/components/Nav2.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import { Button, Container, Form, Nav, NavDropdown, Navbar, Offcanvas } from 'react-bootstrap';
+import { NavLink, useNavigate } from 'react-router-dom';
+import '../css/Nav.css'; // Import file CSS untuk mengatur tampilan
+import { useDispatch, useSelector } from 'react-redux';
 import { LogOut, reset, getMe } from "../features/authSlice";
-import { Container, Navbar, Form, Button, NavDropdown } from "react-bootstrap";
-import { NavLink, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
+import axios from 'axios';
 
-const Nav = () => {
+const navbar = () => {
+  const expand = 'md';
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.auth);
@@ -34,97 +36,95 @@ const Nav = () => {
   };
 
   return (
-    <div className="mb-5 ">
-      <Navbar
-        fixed="top"
-        collapseOnSelect
-        expand="lg"
-        bg="light"
-        variant="light"
-      >
-        <Container>
-          <NavLink style={{ textDecoration: "none", color: "Black" }} to={"/"}>
-            AppKost
-          </NavLink>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse
-            id="responsive-navbar-nav"
-            className="justify-content-end"
-          >
-            <NavLink
-              style={{
-                textDecoration: "none",
-                paddingRight: 15,
-                color: "Black",
-              }}
-              className="mx-2"
-              to={"/pusat-bantuan"}
-            >
-              Pusat Bantuan
-            </NavLink>
-            <NavLink
-              style={{
-                textDecoration: "none",
-                paddingRight: 15,
-                color: "Black",
-              }}
-              className="mx-2"
-              to={"/tentang-kami"}
-            >
-              Tentang Kami
-            </NavLink>
-            <NavLink
-              style={{
-                textDecoration: "none",
-                paddingRight: 15,
-                color: "Black",
-              }}
-              className="mx-2"
-              to={"/maps"}
-            >
-              Maps
-            </NavLink>
-            <NavLink
-              style={{
-                textDecoration: "none",
-                paddingRight: 15,
-                color: "Black",
-              }}
-              className="mx-2"
-              to={"/kost-list"}
-            >
-              List Rumah Kost
-            </NavLink>
-            <Form className="d-flex">
-              {user == null ? (
-                <Button onClick={login} variant="outline-success">
-                  Login
-                </Button>
-              ) : (
-                <>
-                  <NavDropdown title={user.name} id="collasible-nav-dropdown">
-                    <NavDropdown.Item
-                      onClick={dashboard}
-                      variant="outline-success"
-                    >
-                      Akun
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item
-                      onClick={logout}
-                      variant="outline-success"
-                    >
-                      Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </>
-              )}
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <Navbar fixed="top" expand={expand} className="bg-body-tertiary mb-3">
+      <Container style={{ maxWidth: "1250px" }}>
+        <NavLink style={{ textDecoration: "none", paddingRight: 15, color: "Black" }} href="/">AppKost</NavLink>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-${expand}`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+              Offcanvas
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="d-md-flex flex-md-row d-block flex-column ml-auto"> {/* Tambahkan kelas justify-content-end pada elemen Nav */}
+              <Nav.Link
+                as={NavLink}
+                style={{
+                  textDecoration: "none",
+                  paddingRight: 15,
+                  color: "Black",
+                }}
+                to={"/pusat-bantuan"}
+              >
+                Pusat Bantuan
+              </Nav.Link>
+              <Nav.Link
+                as={NavLink}
+                style={{
+                  textDecoration: "none",
+                  paddingRight: 15,
+                  color: "Black",
+                }}
+                to={"/tentang-kami"}
+              >
+                Tentang Kami
+              </Nav.Link>
+              <Nav.Link
+                style={{
+                  textDecoration: "none",
+                  paddingRight: 15,
+                  color: "Black",
+                }}
+                to={"/maps"}
+              >
+                Maps
+              </Nav.Link>
+              <Nav.Link
+                style={{
+                  textDecoration: "none",
+                  paddingRight: 15,
+                  color: "Black",
+                }}
+                to={"/kost-list"}
+              >
+                List Rumah Kost
+              </Nav.Link>
+              <Form className="d-flex">
+                {user == null ? (
+                  <Button onClick={login} variant="outline-success">
+                    Login
+                  </Button>
+                ) : (
+                  <>
+                    <NavDropdown title={user.name} id="collasible-nav-dropdown">
+                      <NavDropdown.Item
+                        onClick={dashboard}
+                        variant="outline-success"
+                      >
+                        Akun
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item
+                        onClick={logout}
+                        variant="outline-success"
+                      >
+                        Logout
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                  </>
+                )}
+              </Form>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Nav;
+export default navbar;
